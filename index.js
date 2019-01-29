41d277f86aa8a9e071fa70f23314b893e5ddfb51
Érick <erickcontato@hotmail.com>
2019-01-29 09:46:55 -0200

    modified:   index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var app = require('./config/express')();
 var config = require('./config/environment');
 require('dotenv').config();
 app.listen(config.port, function () {
-    console.log('Servidor rodando!');
+    console.log('Dont Panic - Sua aplicação de ML está no ar!');
 })
